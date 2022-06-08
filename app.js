0f4c6a5179a99560279ee3d2188408cb0c7af37b
Gabrielle Brumer <96775611+gabiBrumer@users.noreply.github.com>
2022-06-08 00:39:42 -0300

    Modificação no arquivo perfilCliente EJS na pasta view, cricação de formulário e CSS

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const porta = 3000;
 const homeRouter = require('./src/routes/homeRouter')
 const userRouter = require('./src/routes/userRouter')
 const clienteRouter = require ('./src/routes/clienteRouter');
 
 app.set('view engine', 'ejs')
-app.set('views', './src/views')
+app.set ('views', 'src/views')
 
 app.use(express.static('public')) 
 
@@ -14,6 +14,6 @@ app.use(homeRouter)
 app.use(userRouter)
 app.use(clienteRouter);
 
-app.listen(port, () => {
-    console.log('listening on port: ' + port)
+app.listen(porta, () => {
+    console.log('listening on porta: ' + porta)
 })
