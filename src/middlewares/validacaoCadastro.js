135dba7316a49af5df31750a67957ce97a83696a
jrbigmon <junioterriaga0@gmail.com>
2022-07-17 17:10:51 -0300

    finalizado back de cadastro com middlewares, rotas, controllers e ejs

--- src/middlewares/validacaoCadastro.js
+++ src/middlewares/validacaoCadastro.js
@@ -3,7 +3,7 @@ const {body} = require('express-validator')
 const validacaoCadastro = [
     body('nome').notEmpty().withMessage('Insira um nome!'),
     body('sobrenome').notEmpty().withMessage('Insira um sobrenome!'),
-    body('data_nascimeno').notEmpty().withMessage('Insira sua data de nascimento!'),
+    body('data_nascimento').notEmpty().withMessage('Insira sua data de nascimento!'),
     body('cep').notEmpty().withMessage('Insira seu CEP!'),
     body('telefone').notEmpty().withMessage('Insira seu telefone!'),
     body('email').notEmpty().withMessage('Insira um email válido!'),
