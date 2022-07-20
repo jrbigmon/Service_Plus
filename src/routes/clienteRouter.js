76015c2a14c12f361eff77570924aa25fdd75c71
jrbigmon <junioterriaga0@gmail.com>
2022-07-20 11:31:44 -0300

    iniciado alterações na busca por profissionais

--- src/routes/clienteRouter.js
+++ src/routes/clienteRouter.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const clienteController = require ('../controllers/clienteController')
 
-router.get('/perfil/cliente', clienteController.index);
-router.get('/perfil/cliente/cadastrar', clienteController.showCadastrar);
 router.get('/perfil/cliente/buscar', clienteController.showBuscar);
 router.get('/perfil/cliente/profissionais', clienteController.showProfissionais);
 router.get('/perfil/cliente/profissional/', clienteController.showProfissional); // arrumar rota: '/perfil/cliente/profissional/:id'
