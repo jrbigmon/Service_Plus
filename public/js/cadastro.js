00ac3e377d72c28b86c3a8204da47f1938e28de8
jrbigmon <junioterriaga0@gmail.com>
2022-08-03 21:55:27 -0300

    realizado validação da tela de cadastro não permitindo dar submit

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,11 +1,48 @@
-let checkbox = document.getElementById('checkbox-profissional');
-let select = document.getElementById('select-areas');
 
-checkbox.addEventListener('click', () => {
-    if(checkbox.checked) {
-        select.removeAttribute('hidden');
-    }
-    else {
-        select.setAttribute('hidden', 'hidden')
-    }
+window.addEventListener('load', () => {
+    let checkbox = document.getElementById('checkbox-profissional');
+    let select = document.getElementById('select-areas');
+    let form = document.querySelector('.meu-form');
+    let inputs = document.querySelectorAll('input');
+
+    checkbox.addEventListener('click', () => {
+        if(checkbox.checked) {
+            select.removeAttribute('hidden');
+        }
+        else {
+            select.setAttribute('hidden', 'hidden')
+        }
+    });
+
+    form.addEventListener('submit', (event) => {
+        let errors = []
+
+        for (let input of inputs) {
+            
+            if(input.type !== 'checkbox') {
+                let campo = input.value.trim();
+
+                if(campo.length == 0){
+                    errors.push(` ${input.name}`);
+                    
+                    input.style.border = '1px solid red';
+
+                } else {
+                    input.style.border = '1px solid green'
+                }
+            }
+        }
+        if(errors.length > 0){
+            Swal.fire({
+                icon: 'warning',
+                title: 'Oops...',
+                text: 'Possuem campos Vazios!',
+                footer: '<a href="">Why do I have this issue?</a>'
+              })
+            event.preventDefault();
+        }
+    })
+
+
 })
+
