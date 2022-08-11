6c2c391d048c1a637a53ca95bfbb574dfe47df25
jrbigmon <junioterriaga0@gmail.com>
2022-08-11 18:21:21 -0300

    aplicado standard js

--- src/middlewares/userVerifyExists.js
+++ src/middlewares/userVerifyExists.js
@@ -2,25 +2,25 @@ const { Cliente, Profissional } = require('../database/models')
 const bcrypt = require('bcryptjs')
 
 const userVerifyExists = async (req, res, next) => {
-    const { email, senha } = req.body
-    const { usuario } = req.query
+  const { email, senha } = req.body
+  const { usuario } = req.query
 
-    if(usuario === "cliente"){
-        const cliente = await Cliente.findOne({where: { email }})
-        if (!cliente || !bcrypt.compareSync(senha, cliente.senha)){
-            res.locals.errorValidation = "Tem erro nessa bagaça"
-            return next()
-        }
-        return next()
+  if (usuario === 'cliente') {
+    const cliente = await Cliente.findOne({ where: { email } })
+    if (!cliente || !bcrypt.compareSync(senha, cliente.senha)) {
+      res.locals.errorValidation = 'Tem erro nessa bagaça'
+      return next()
     }
-    if(usuario === "profissional"){
-        const profissional = await Profissional.findOne({where: { email }})
-        if (!profissional || !bcrypt.compareSync(senha, profissional.senha)){
-            res.locals.errorValidation = "Tem erro nessa bagaça"
-            return next()
-        }
-        return next()
+    return next()
+  }
+  if (usuario === 'profissional') {
+    const profissional = await Profissional.findOne({ where: { email } })
+    if (!profissional || !bcrypt.compareSync(senha, profissional.senha)) {
+      res.locals.errorValidation = 'Tem erro nessa bagaça'
+      return next()
     }
+    return next()
+  }
 }
 
 module.exports = userVerifyExists
