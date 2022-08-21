99323d7c80a0c15740ff6fe821a06f8a7e2041fd
jrbigmon <junioterriaga0@gmail.com>
2022-08-21 18:16:09 -0300

    Criado rota para o estado do servico executar

--- src/routes/servicoRouter.js
+++ src/routes/servicoRouter.js
@@ -7,6 +7,7 @@ router.use(auth)
 router.get('/servicos/:id/solicitar', servicoController.viewOrcamentoByClient)
 router.post('/servicos/:id/solicitar', servicoController.solicitarOrcamentoByClient)
 router.put('/servicos/:id/orcamentar', servicoController.orcamentarByProfessional)
+router.put('/servicos/:id/finalizar', servicoController.servicoExecutadoByProfissional)
 router.put('/servicos/:id/aceitar', servicoController.aceitarServicoOrcadoClient)
 router.put('/servicos/:id/cancelar', servicoController.cancelarServico)
 module.exports = router
