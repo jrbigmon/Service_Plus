643d54f723ed9d3ba87552f44a300e84ee2f6b8e
jrbigmon <junioterriaga0@gmail.com>
2022-08-22 16:41:50 -0300

    Criado middleware para alteração de status do serviço para automatico se a data atual for maior que a data do serviço

---
+++ src/middlewares/changeStatusServiceAuto.js
@@ -0,0 +1,30 @@
+const moment = require('moment')
+const { ClienteHasProfissional } = require('../database/models')
+
+const dataAtual = moment().format('YYYYMMDD')
+
+const changeStatusServiceAuto = async (req, res, next) => {
+    if(req.session.countUpdateStatusService) return next()
+
+    const servicos = await ClienteHasProfissional.findAll()
+
+    for (let servico of servicos){
+        const dataServico = servico.dataServico
+
+        const dataServicoFormat = moment(dataServico, 'YYYY-MM-DD').format('YYYYMMDD')
+
+        if(dataServicoFormat - dataAtual < 0){
+            const servicoAtualizado = {
+                ...servico,
+                situacaoServicoId: 5
+            }
+
+            ClienteHasProfissional.update(servicoAtualizado, {where: {id: servico.id}}) 
+        }
+    }
+
+    req.session.countUpdateStatusService = 'Já verificado nessa sessão!'
+
+    return next()
+}
+module.exports = changeStatusServiceAuto
