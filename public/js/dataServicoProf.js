cc6d51633218367067afc48a53a1a4eccbcbe4b6
Gabrielle Brumer <gabriellebrumer@hotmail.com>
2022-08-23 19:42:01 -0300

    Formatação de data nos históricos de profissional e cliente

--- public/js/dataServicoProf.js
+++ public/js/dataServicoProf.js
@@ -1,13 +1,11 @@
 window.addEventListener('load', () => {
     const datasAtuais = document.querySelectorAll('.date-br');
+
     for (let data of datasAtuais) {
-        const dataNova = new Date(data);
+        const dataNova = new Date(data.innerHTML);
         const formatar = Intl.DateTimeFormat('pt-BR', {
             dateStyle: 'short'
         })
-        // data.innerHTML = formatar.format(dataNova);
-
-        console.log(data)
-        console.log(dataNova)
+        data.innerHTML = formatar.format(dataNova);
     }
 })
