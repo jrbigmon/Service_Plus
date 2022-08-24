64de69afa79568a250e2a0f61f1a5c89a5a40169
jrbigmon <junioterriaga0@gmail.com>
2022-08-24 12:00:58 -0300

    Adicionado Glider.js ao projeto para realizar o carrossel

---
+++ public/js/glider.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.dots',
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        }
+      });
+      console.log('cheguei')
+})
