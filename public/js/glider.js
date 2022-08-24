0cb0002b2abee3572f8d49926cf9e05181fedb14
jrbigmon <junioterriaga0@gmail.com>
2022-08-24 12:13:04 -0300

    Removido console de teste

--- public/js/glider.js
+++ public/js/glider.js
@@ -1,13 +1,12 @@
 window.addEventListener('load', () => {
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: '.dots',
-        arrows: {
-          prev: '.glider-prev',
-          next: '.glider-next'
-        }
-      });
-      console.log('cheguei')
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
 })
