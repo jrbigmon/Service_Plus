80f4997dd7d526439feeadb902e43010232fbbad
jrbigmon <junioterriaga0@gmail.com>
2022-08-24 14:43:39 -0300

    alterado validação para caso o valor da api venha como undefined

--- public/js/historicoServicoProf.js
+++ public/js/historicoServicoProf.js
@@ -4,8 +4,12 @@ window.addEventListener('load', async () => {
     for (let cep of cepsToApi) {
         const response = await axios(`https://viacep.com.br/ws/${cep.innerHTML}/json`)
         let endereco = response.data.logradouro
-        cep.innerHTML = 'End: ' + endereco
-    }
+        if(endereco) {
+            cep.innerHTML = 'End: ' + endereco
+        } else {
+            cep.innerHTML = 'Cep: ' + cep.innerHTML
+        }   
 
+    }
 
 })
