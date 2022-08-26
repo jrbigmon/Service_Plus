9428224d5cb7419fad7fac7c3bb8a25896fa6b9c
jrbigmon <junioterriaga0@gmail.com>
2022-08-25 21:29:56 -0300

    numero de elementos na tela

--- public/js/glider.js
+++ public/js/glider.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
   new Glider(document.querySelector('.glider'), {
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     draggable: true,
     dots: '.dots',
