6477960757073bb784400ca00605d24ce60527b8
jrbigmon <junioterriaga0@gmail.com>
2022-08-30 12:15:51 -0300

    Modificação do controller para data e js da api para cada modificação fazer request

--- public/js/editPerfilCliente.js
+++ public/js/editPerfilCliente.js
@@ -1,6 +1,8 @@
-window.addEventListener('load', async () => {
+window.addEventListener('load', () => {
     const cep = document.querySelector('#cep')
-    const endereco = document.querySelector('#endereco')
-    const response = await axios(`https://viacep.com.br/ws/${cep.value}/json`)
-    endereco.value = response.data.logradouro
+    cep.addEventListener('change', async() => {
+        const endereco = document.querySelector('#endereco')
+        const response = await axios(`https://viacep.com.br/ws/${cep.value}/json`)
+        endereco.value = response.data.logradouro
+    })
 })
